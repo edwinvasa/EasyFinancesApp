48f06a116c9fb1bf5549aa743cf90d9472db1baf
edwinvasa <edwinvasa@gmail.com>
2024-09-27 20:14:14 -0500

    updated files to github-pages

--- docs/chunk-ISMOAKWZ.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-SAPECVW5.js";import"./chunk-6OQHB22I.js";export{a as GestionCreditoModule};
---
+++ docs/chunk-KMIBP3OX.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-L4MFIWXT.js";import"./chunk-6OQHB22I.js";export{a as GestionCreditoModule};
