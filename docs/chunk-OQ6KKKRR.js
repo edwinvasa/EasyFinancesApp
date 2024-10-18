28f9b9d65c464fb1c24c7b035f0857bdcbd9cb1c
edwin vanegas <edwin.vanegas@rappi.com>
2024-10-18 14:05:05 -0500

    mejora en tasas de interes de los bancos

---
+++ docs/chunk-OQ6KKKRR.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-AI3DL7TN.js";import"./chunk-AX6OQGLD.js";export{a as EconomicAnalysisModule};
--- docs/chunk-U3C4FKBP.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-5A3J2GLV.js";import"./chunk-AX6OQGLD.js";export{a as EconomicAnalysisModule};
