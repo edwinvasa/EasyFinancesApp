6e25ccad5e47a2e3074b1361c30a9f5100279216
edwinvasa <edwinvasa@gmail.com>
2024-11-03 00:09:01 -0500

    fix toggleAmortizationTables

---
+++ docs/chunk-AX7FOM5P.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-WNZXKSQU.js";import"./chunk-KXCEMWHI.js";export{a as GestionCreditoModule};
--- docs/chunk-TXFSXWZH.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-ZZBDWRCE.js";import"./chunk-KXCEMWHI.js";export{a as GestionCreditoModule};
